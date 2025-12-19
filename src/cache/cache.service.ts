c79670e4fe91f13b55fd94b7766499ab1e94fff0
Arthur Sampaio <arthurr_sampaio@hotmail.com>
2025-12-18 22:53:37 -0300

    feat: added caching module

---
+++ src/cache/cache.service.ts
@@ -0,0 +1,23 @@
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CacheService {
+    constructor (
+       @Inject(CACHE_MANAGER) private cacheManager: Cache 
+    ) {}
+
+    async getCache<T>(key: string, functionRequest: () => Promise<T>): Promise<T> {
+        const allData = await this.cacheManager.get<T>(key);
+
+        if (allData) {
+            return allData;
+        }
+
+        const call: T = await functionRequest();
+
+        await this.cacheManager.set(key, call);
+
+        return call;
+    }
+}
--- src/city/city.service.ts
+++ src/city/city.service.ts
@@ -1,8 +1,8 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { CityEntity } from './entities/city.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
+import { CacheService } from 'src/cache/cache.service';
 
 @Injectable()
 export class CityService {
@@ -10,20 +10,11 @@ export class CityService {
     constructor(
         @InjectRepository(CityEntity)
         private readonly cityRepository: Repository<CityEntity>,
-        @Inject(CACHE_MANAGER) private cacheManager: Cache
+
+        private readonly cacheService: CacheService
     ) {}
 
     async getAllCitiesByStateId(stateId: number): Promise<CityEntity[]> {
-        const citiesCache = await this.cacheManager.get<CityEntity[]>(`state_${stateId}`);
-
-        if (citiesCache) {
-            return citiesCache
-        }
-
-        const cities = await this.cityRepository.find({ where: { stateId } });
-
-        await this.cacheManager.set(`state_${stateId}`, cities);
-
-        return cities;
+        return this.cacheService.getCache<CityEntity[]>(`state_${stateId}`, () => this.cityRepository.find({ where: { stateId } }))
     }
 }
