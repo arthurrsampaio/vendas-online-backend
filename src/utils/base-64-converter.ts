0404e3f9838d1c3a88415e4c13e50ee9461349c6
arthurrsampaio <arthurr_sampaio@hotmail.com>
2026-01-25 16:08:41 -0300

    feat: created userId decorator

---
+++ src/utils/base-64-converter.ts
@@ -0,0 +1,15 @@
+import { LoginPayload } from 'src/auth/dtos/loginPayload.dto';
+
+export const authorizationToLoginPayload = (
+  authorization: string,
+): LoginPayload | undefined => {
+  const authorizationSplitted = authorization.split('.');
+
+  if (authorizationSplitted.length < 3 || !authorizationSplitted[1]) {
+    return undefined;
+  }
+
+  return JSON.parse(
+    Buffer.from(authorizationSplitted[1], 'base64').toString('ascii'),
+  );
+};
