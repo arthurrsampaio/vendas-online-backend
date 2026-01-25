55c70f28b284636e292cb174d0abde31157546cd
arthurrsampaio <arthurr_sampaio@hotmail.com>
2026-01-25 14:54:09 -0300

    feat: created jwt user validation

--- src/address/address.controller.ts
+++ src/address/address.controller.ts
@@ -1,17 +1,28 @@
-import { Body, Controller, Param, Post, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { CreateAddressDto } from './dtos/createAddress.dto';
 import { AddressService } from './address.service';
 import { AddressEntity } from './entities/address.entity';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from 'src/user/enum/user-type.enum';
 
+@Roles(UserType.User)
 @Controller('address')
 export class AddressController {
+  constructor(private readonly addressService: AddressService) {}
 
-    constructor(private readonly addressService: AddressService) {}
-
-    @Post('/:userId')
-    @UsePipes(ValidationPipe)
-    async createAddress(
-        @Body() CreateAddressDto: CreateAddressDto, @Param('userId') userId: number): Promise<AddressEntity> {
-        return this.addressService.createAddress(CreateAddressDto, userId);
-    }
+  @Post('/:userId')
+  @UsePipes(ValidationPipe)
+  async createAddress(
+    @Body() CreateAddressDto: CreateAddressDto,
+    @Param('userId') userId: number,
+  ): Promise<AddressEntity> {
+    return this.addressService.createAddress(CreateAddressDto, userId);
+  }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -7,29 +7,40 @@ import { CityModule } from './city/city.module';
 import { AddressModule } from './address/address.module';
 import { CacheModule } from './cache/cache.module';
 import { AuthModule } from './auth/auth.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './guards/roles.guard';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule.forRoot({
-    envFilePath: ['.env.development.local'],
-  }),
-  TypeOrmModule.forRoot({
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    password: process.env.DB_PASSWORD,
-    port: Number(process.env.DB_PORT),
-    username: process.env.DB_USERNAME,
-    type: 'postgres',
-    entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-    migrations: [`${__dirname}/migration/{.ts,*.js}`],
-    migrationsRun: true,
-  }),
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ['.env.development.local'],
+    }),
+    TypeOrmModule.forRoot({
+      database: process.env.DB_DATABASE,
+      host: process.env.DB_HOST,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      type: 'postgres',
+      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+      migrations: [`${__dirname}/migration/{.ts,*.js}`],
+      migrationsRun: true,
+    }),
     UserModule,
     StateModule,
     CityModule,
     AddressModule,
     CacheModule,
-    AuthModule],
+    AuthModule,
+    JwtModule,
+  ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
