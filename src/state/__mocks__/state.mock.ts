14a05e93af039804a11c95bde8644627ba9306cb
Arthur Sampaio <arthurr_sampaio@hotmail.com>
2026-01-29 15:31:55 -0300

    feat: insert unit tests in state and city

---
+++ src/state/__mocks__/state.mock.ts
@@ -0,0 +1,8 @@
+import { StateEntity } from '../entities/state.entity';
+
+export const stateMock: StateEntity = {
+  createdAt: new Date(),
+  id: 465151,
+  name: 'stateName',
+  updatedAt: new Date(),
+};
