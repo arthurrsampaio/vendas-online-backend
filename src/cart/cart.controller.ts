34928a57598a686b228cc506eb6d98cb3caf395d
Arthur Sampaio <arthurr_sampaio@hotmail.com>
2026-05-01 13:37:09 -0300

    update

--- src/cart/cart.controller.ts
+++ src/cart/cart.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { Roles } from '../decorators/roles.decorator';
 import { UserType } from '../user/enum/user-type.enum';
+import { CartEntity } from './entities/cart.entity';
+import { InsertCartDto } from './dtos/insert-cart.dto';
 
 @Roles(UserType.User)
 @Controller('cart')
-export class CartController {}
+export class CartController {
+  @UsePipes(ValidationPipe)
+  @Post()
+  async createCart(@Body() insertCart: InsertCartDto): Promise<CartEntity> {}
+}
--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CartEntity } from './entities/cart.entity';
 import { Repository } from 'typeorm';
+import { InsertCartDto } from './dtos/insert-cart.dto';
 
 @Injectable()
 export class CartService {
@@ -9,4 +10,8 @@ export class CartService {
     @InjectRepository(CartEntity)
     private readonly cartRepository: Repository<CartEntity>,
   ) {}
+
+  async verifyActiveCart() {}
+
+  async createCart(insertCart: InsertCartDto): Promise<CartEntity> {}
 }
