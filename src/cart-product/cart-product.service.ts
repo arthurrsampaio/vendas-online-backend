f5bfcf3866e908af7a6f55a8eb296fca558028bd
Arthur Sampaio <arthurr_sampaio@hotmail.com>
2026-05-18 22:40:37 -0300

    feat: verify productId and created ReturnCartDto

--- src/cart-product/cart-product.service.ts
+++ src/cart-product/cart-product.service.ts
@@ -4,12 +4,14 @@ import { CartProductEntity } from './entities/cart-product.entity';
 import { Repository } from 'typeorm';
 import { InsertCartDto } from '../cart/dtos/insert-cart.dto';
 import { CartEntity } from '../cart/entities/cart.entity';
+import { ProductService } from '../product/product.service';
 
 @Injectable()
 export class CartProductService {
   constructor(
     @InjectRepository(CartProductEntity)
     private readonly cartProductRepository: Repository<CartProductEntity>,
+    private readonly productService: ProductService,
   ) {}
 
   async verifyProduct(
@@ -45,6 +47,8 @@ export class CartProductService {
     insertCartDto: InsertCartDto,
     cart: CartEntity,
   ): Promise<CartProductEntity> {
+    await this.productService.findProductById(insertCartDto.productId);
+
     const cartProduct = await this.verifyProduct(
       insertCartDto.productId,
       cart.id,
