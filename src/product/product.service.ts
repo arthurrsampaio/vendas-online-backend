7819676a5bfe484f1c4df4ded3fdd7a9cd48f205
Arthur Sampaio <arthurr_sampaio@hotmail.com>
2026-08-26 12:25:03 -0300

    feat: find product and insert orderProduct using promise

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ProductEntity } from './entities/product.entity';
-import { DeleteResult, Repository } from 'typeorm';
+import { DeleteResult, In, Repository } from 'typeorm';
 import { CreateProductDto } from './dtos/create-product.dto';
 import { CategoryService } from '../category/category.service';
 import { UpdateProductDto } from './dtos/update-product.dto';
@@ -15,8 +15,18 @@ export class ProductService {
     private readonly categoryService: CategoryService,
   ) {}
 
-  async getAllProducts(): Promise<ProductEntity[]> {
-    const products = await this.productRepository.find();
+  async getAllProducts(productId?: number[]): Promise<ProductEntity[]> {
+    let findOptions = {};
+
+    if (productId && productId.length > 0) {
+      findOptions = {
+        where: {
+          id: In(productId),
+        },
+      };
+    }
+
+    const products = await this.productRepository.find(findOptions);
 
     if (!products || products.length === 0) {
       throw new NotFoundException('No product listed');
